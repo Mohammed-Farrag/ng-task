7678899ca7bd91761cd580ea62c68dee522dad1f
Mohammed-Farrag <memothetop633@gmail.com>
2023-10-07 11:47:53 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { GraphViewComponent } from './components/graph-view/graph-view.component
 import { ListViewComponent } from './components/list-view/list-view.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'graph', pathMatch: 'full'},
   {path: 'graph', component:  GraphViewComponent},
   {path: 'list', component:  ListViewComponent},
 ];
